3c566205c39ef55b5ac8c2e2fa5ae5fc6b2846f5
EFFECT <info@effect.moe>
2023-07-31 03:22:57 +0900

    remotefolio

--- next.config.js
+++ next.config.js
@@ -21,16 +21,7 @@ module.exports = withBundleAnalyzer({
   }
 })
 
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-    buildExcludes: [/middleware-manifest.json$/],  // 追加
-  },
-})
 
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
