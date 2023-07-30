8b4f004249b31c86bd9ccaf517704dcb516d9664
EFFECT <info@effect.moe>
2023-07-30 20:55:32 +0900

    remotefolio

--- next.config.js
+++ next.config.js
@@ -21,15 +21,4 @@ module.exports = withBundleAnalyzer({
   }
 })
 
-/** @type {import('next').NextConfig} */
-
-const withPWA = require('next-pwa');
-
-module.exports = withPWA({
-  reactStrictMode: true,
-  pwa: {
-    dest: 'public',
-    register: true,
-    disable: process.env.NODE_ENV === 'development',
-  },
-});
+buildExcludes: [/manifest.json$/]  // 追加
