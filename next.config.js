002fa103f16517cc4332906023f5f8afb4195506
EFFECT <info@effect.moe>
2023-07-31 18:36:56 +0900

    remotefolio

--- next.config.js
+++ next.config.js
@@ -25,14 +25,8 @@ module.exports = withBundleAnalyzer({
 
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
-  // disable: process.env.NODE_ENV === "development",
-  // register: true,
-  // scope: "/app",
-  // sw: "service-worker.js",
-  //...
 });
 
-// Your Next config is automatically typed!
 module.exports = withPWA({
   // Your Next.js config
 });
