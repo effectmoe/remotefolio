1de0d42ba2010ab11b1628b9aaf09fa3a06fe3dc
EFFECT <info@effect.moe>
2023-08-14 21:57:02 +0900

    8.14

---
+++ .next/server/pages/404.js
@@ -0,0 +1,65 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 197;
+exports.ids = [197];
+exports.modules = {
+
+/***/ 5838:
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.a(module, async (__webpack_handle_async_dependencies__, __webpack_async_result__) => { try {
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _components_Page404__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2484);
+var __webpack_async_dependencies__ = __webpack_handle_async_dependencies__([_components_Page404__WEBPACK_IMPORTED_MODULE_0__]);
+_components_Page404__WEBPACK_IMPORTED_MODULE_0__ = (__webpack_async_dependencies__.then ? (await __webpack_async_dependencies__)() : __webpack_async_dependencies__)[0];
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_components_Page404__WEBPACK_IMPORTED_MODULE_0__/* .Page404 */ .l);
+
+__webpack_async_result__();
+} catch(e) { __webpack_async_result__(e); } });
+
+/***/ }),
+
+/***/ 968:
+/***/ ((module) => {
+
+module.exports = require("next/head");
+
+/***/ }),
+
+/***/ 6689:
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ }),
+
+/***/ 8751:
+/***/ ((module) => {
+
+module.exports = import("notion-utils");;
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [872,3,484], () => (__webpack_exec__(5838)));
+module.exports = __webpack_exports__;
+
+})();
