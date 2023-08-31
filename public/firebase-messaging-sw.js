570371417c9d5c614e63be3ebf16d0787df6b330
EFFECT <info@effect.moe>
2023-08-31 14:31:56 +0900

    8.31

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,7 +1,6 @@
 
 importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
-importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyCGYS03THZx5MXHuQt8xZ2Zm3y-SepPKnQ",
@@ -11,10 +10,9 @@ firebase.initializeApp({
   messagingSenderId: "53262820382",
   appId: "1:53262820382:web:f57d57669635f114b0ba0f"
 });
-
-const firestore = firebase.firestore();
 const messaging = firebase.messaging();
-messaging.onBackgroundMessage((payload) => {
+
+messaging.onBackgroundMessage(async (payload) => {
   console.log(
     "[firebase-messaging-sw.js] Received background message ",
     payload
@@ -22,18 +20,20 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.data?.title;
   const notificationOptions = {
       body: payload.data?.body,
-      icon: payload.data?.image
+      icon: payload.data?.image,
+      data: payload.data,
   };
-  console.log(`firebase onBackgroundMessage.`)
+  console.log(`firebase onBackgroundMessage.`);
   if (payload.data.nid && payload.data.uid) {
-    firestore.collection('notifications').doc(payload.data.nid).update({recivepersons: firestore.FieldValue.arrayUnion(payload.data.uid)});
+    await fetch('https://asia-northeast1-pwapushtest-ca52a.cloudfunctions.net/recivenotification', { method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({ nid: payload.data.nid, uid: payload.data.uid, }) });
   }
   return self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-self.addEventListener('notificationclick', (event) => {
-  if (event.notification.data.nid && event.notification.data.uid) {
-    firestore.collection('notifications').doc(event.notification.data.nid).update({openpersons: firestore.FieldValue.arrayUnion(event.notification.data.uid)});
+self.addEventListener('notificationclick', async (event) => {
+  console.log('click notify', event);
+  if (event.notification.data && event.notification.data.nid && event.notification.data.uid) {
+    await fetch('https://asia-northeast1-pwapushtest-ca52a.cloudfunctions.net/opennotification', { method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({ nid: event.notification.data.nid, uid: event.notification.data.uid, }) });
   }
-  event.notification.close()
+  event.notification.close();
 });
